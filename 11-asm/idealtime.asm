797d5a4f65e936b41d9a3b5acfde163e3f105734
sokanaid <71149930+sokanaid@users.noreply.github.com>
2021-11-25 21:20:18 +0500

    Add files via upload

---
+++ 11-asm/idealtime.asm
@@ -0,0 +1,377 @@
+;------------------------------------------------------------------------------
+; perimeter.asm - единица компиляции, вбирающая функции вычисления идеального времени и сортировки контейнера
+;------------------------------------------------------------------------------
+extern printf
+extern TRAIN
+extern SHIP
+extern PLANE
+extern OutTransport
+;----------------------------------------------
+; Вычисление идеального времени поезда
+
+global IdealTimeTrain
+IdealTimeTrain:
+section .bss
+    d       resq    1
+
+section .text
+push rbp
+mov rbp, rsp
+
+    ; В rdi адрес поезда
+    mov     eax, [rdi+4]
+    cvtsi2sd    xmm0, eax
+    movsd [d], xmm0
+    movsd   xmm0,[rdi+8]
+    movsd   xmm1, [d]
+    divsd    xmm0, xmm1
+
+leave
+ret
+
+;----------------------------------------------
+; double IdealTimeShip(void *t) {
+;    return (double)(*((int*)t)
+;       + *((int*)(t+intSize))
+;       + *((int*)(t+2*intSize)));
+;}
+global IdealTimeShip
+IdealTimeShip:
+section .bss
+    .d       resq    1
+
+section .text
+push rbp
+mov rbp, rsp
+
+    ; В rdi адрес ship
+    mov     eax, [rdi+8]
+    cvtsi2sd    xmm0, eax
+    movsd [.d], xmm0
+    movsd   xmm0,[rdi+12]
+    movsd   xmm1, [.d]
+    divsd    xmm0, xmm1
+
+leave
+ret
+
+global IdealTimePlane
+IdealTimePlane:
+section .bss
+    .d       resq    1
+
+section .text
+push rbp
+mov rbp, rsp
+
+    ; В rdi адрес ship
+    mov     eax, [rdi+8]
+    cvtsi2sd    xmm0, eax
+    movsd [.d], xmm0
+    movsd   xmm0,[rdi+12]
+    movsd   xmm1, [.d]
+    divsd    xmm0, xmm1
+
+leave
+ret
+;----------------------------------------------
+; Вычисление периметра фигуры
+;double IdealTimeTransport(void *s) {
+;    int k = *((int*)s);
+;    if(k == RECTANGLE) {
+;        return PerimeterRectangle(s+intSize);
+;    }
+;    else if(k == TRIANGLE) {
+;        return IdealTimeShip(s+intSize);
+;    }
+;    else {
+;        return 0.0;
+;    }
+;}
+global IdealTimeTransport
+IdealTimeTransport:
+section .text
+push rbp
+mov rbp, rsp
+
+    ; В rdi адрес фигуры
+    mov eax, [rdi]
+    cmp eax, [TRAIN]
+    je idealTimeTrain
+    cmp eax, [SHIP]
+    je idealTimeship
+    cmp eax, [PLANE]
+    je idealTimePlane
+    xor eax, eax
+    cvtsi2sd    xmm0, eax
+    jmp     return
+idealTimeTrain:
+    ; Вычисление идеального времени поезда
+    add     rdi, 4
+    call    IdealTimeTrain
+    jmp     return
+idealTimeship:
+    ; Вычисление идеального времени корабля.
+    add     rdi, 4
+    call    IdealTimeShip
+    jmp     return
+idealTimePlane:
+    ; Вычисление идеального времени корабля.
+    add     rdi, 4
+    call    IdealTimePlane
+    jmp     return
+return:
+leave
+ret
+
+;----------------------------------------------
+;// Сортировка Шелла
+;string ShellSort(container &c) {
+;    int first = 0, last = c.len;
+;    for (auto d = (last - first) / 2; d != 0; d /= 2)
+;        for (auto i = first + d; i != last; ++i)
+;            for (auto j = i; j - first >= d && IdealTime(*c.cont[j]) < IdealTime(*c.cont[(j - d)]); j -= d) {
+;                auto tmp = *c.cont[j];
+;                *c.cont[j] = *c.cont[(j - d)];
+;                *c.cont[(j - d)] = tmp;
+;            }
+;    return "Container was sorted.";
+;}
+global ShellSortContainer
+ShellSortContainer:
+section .data
+    .sum    dq  0
+    .outf db "first = %d,second = %d %d",10,0
+    .outf1 db "d = %d",10,0
+    .outf2 db "rdi1 = %d , rdi2 = %d",10,0
+    .outf3 db "work",10,0
+    .outf4 db "xmm0 = %lg %d, xmm1 = %lg",10,0
+    .outf5 db "rdi = %d, rsi = %d",10,0
+    .zero db 0
+    .two db 24
+    
+    .outf6 db "rsi = %d, rdi = %d ", 10 , 0
+section .bss
+    .first resq 1
+    .last resq 1
+    .len resq 1
+    .d resq 1
+    .i resq 1
+    .j resq 1
+    .g resq 1
+    .start resq 1 ; начало контейнера
+    .idealtime1 resq 1;
+    .idealtime2 resq 1;
+    .adress1 resq 2;
+    .adress2 resq 1;
+section .text
+push rbp
+mov rbp, rsp
+
+    ; В rdi адрес начала контейнера
+    mov [.start], rdi
+    mov [.last], esi            ; число фигур
+    mov esi, 0
+    mov [.first] , esi
+    
+;    mov rdi, .outf
+;    mov rsi,[.first]
+;    mov rdx, [.last]
+;    mov rax, 0
+;    call printf
+    mov rsi, [.last] ;d = (last - first) / 2
+    sub rsi,[.first]
+    sar rsi, 1
+    mov [.d], rsi
+    
+;    mov rdi, .outf1
+;    mov rsi, [.d]
+;    mov rax,0
+;    call printf
+.firstloop: ;for (auto d = (last - first) / 2; d != 0; d /= 2)
+    mov rsi, [.d] ;if(d == 0) break
+    cmp rsi, 0
+    je .return
+    
+    mov rsi, [.first] ;auto i = first + d
+    add rsi, [.d]
+    mov [.i], rsi
+    
+ ;   mov rdi, .outf1
+;    mov rsi, [.i]
+;    mov rax,0
+;    call printf
+    .secondloop: ;for (auto i = first + d; i != last; ++i)
+        mov rsi, [.i] ; if(i == last) break;
+        cmp rsi,[.last]
+        je .continueFirstLoop
+        
+        mov rsi,[.i] ;j = i
+        mov [.j], rsi
+        
+        ;mov rdi, .outf1
+;        mov rsi, [.j]
+;        mov rax,0
+;        call printf
+        .3loop: ;for (auto j = i; j - first >= d && IdealTime(*c.cont[j]) < IdealTime(*c.cont[(j - d)]); j -= d)
+            mov rsi,[.j]     ;if(j - first < d) break
+            sub rsi,[.first]
+            
+;           mov rdi, .outf1
+;            mov rax,0
+;            call printf
+            
+            cmp rsi, [.d]
+            jl .continue2loop
+            
+            mov rdi, [.start];*c.cont[j] in rdi
+            mov rsi, [.j]
+            imul rsi, 24
+            add rdi, rsi
+            mov [.adress1], rdi
+          ;  mov [.start], rdi
+;            mov rdi, .outf3
+;            mov rax, 0
+;            call printf
+;                mov rax , rdi
+;                mov rdi, .outf
+;                mov rsi,[rax]
+;                mov rdx, [rax+4]
+;                mov rcx, [rax+8]
+;                mov rax, 0
+;                call printf
+;                mov rdi, rax
+            
+            ;call OutShape             
+            call IdealTimeTransport ;результат в xmm0
+            
+          ; mov [.start], rdi
+;            mov rdi, .outf3
+;            mov rax, 0
+;            call printf
+            
+            movsd [.idealtime1], xmm0 ; IdealTime(*c.cont[j]) .idealtime1
+            
+            mov rdi, [.start];*c.cont[j-d] in rdi
+            mov rsi, [.j]
+            sub rsi, [.d]
+            imul rsi, 24
+            add rdi, rsi          
+            ;mov r8, [rdi+4]   
+            mov [.adress2], rdi
+            call IdealTimeTransport ;результат в xmm0 IdealTime(*c.cont[(j - d)])
+            
+
+;            mov rdi, .outf4
+;            mov rsi, r8
+;            mov rax, 1
+;            call printf
+            movsd xmm1, xmm0
+            movsd [.idealtime2], xmm0
+            movsd xmm0, [.idealtime1]
+;            
+            ;mov rdi, .outf4
+;            mov rsi, r8
+;            mov rax, 1
+;            call printf
+            
+;            mov rdi, .outf3
+;            mov rax, 0
+;            call printf
+            
+            movsd xmm0, [.idealtime1]
+            movsd xmm1, [.idealtime2]
+
+            cld
+            ;cmpsd
+            ;comiss  xmm0 , xmm1 ;IdealTime(*c.cont[j])? IdealTime(*c.cont[(j - d)])
+            UCOMISD xmm0,xmm1
+            jnb .continue2loop ;if (IdealTime(*c.cont[j]) >= IdealTime(*c.cont[(j - d)])) break;
+
+            ;comiss  xmm0 , xmm1 ;IdealTime(*c.cont[j])? IdealTime(*c.cont[(j - d)])
+            ;je .continue2loop
+            
+
+            ;mov rdi, .outf3
+;            mov rax, 0
+;            call printf
+            ;код обмена
+             
+            ;mov rax, [.adress1]
+;            mov rdi, [rax]
+;            mov rax, [.adress2]
+;            mov rsi, [rax]
+;            mov rsi, rdi
+;            mov rdx, rsi
+;            mov rdi, .outf6
+;            mov rax, 0
+;            call printf
+            
+;            mov rdi, [.adress1]
+;            mov rsi, [.adress2]
+            ;XCHG rdi, rsi
+            mov rax , 0
+            mov [.g] , rax
+            .change:
+                mov rax, [.g]
+                cmp rax, 6
+                je .endchange
+                mov r8, [.g]
+                imul r8, 4
+                
+                mov rax, [.adress1]
+                add rax, r8
+                mov ecx, [rax]
+                
+                mov rax, [.adress2]
+                add rax, r8
+                mov edx, [rax]
+                
+                mov rax, [.adress1]
+                add rax, r8
+                mov [rax],edx
+                
+                mov rax, [.adress2]
+                add rax, r8
+                mov [rax], ecx
+                
+                mov rax, [.g]; g++
+                inc rax
+                mov [.g], rax
+                jmp .change
+            
+            
+            ;код обмена
+            .endchange:
+            mov rsi, [.j];j -= d
+            sub rsi, [.d];
+            mov [.j], rsi
+            jmp .3loop
+        .continue2loop:
+        mov rsi, [.i]; ++i
+        inc rsi
+        mov [.i],rsi
+        jmp .secondloop
+    .continueFirstLoop:
+    mov rsi, [.d];d /= 2
+    sar rsi, 1
+    mov [.d], rsi
+    jmp .firstloop
+; СУММА ПЕРИМЕТРОВ:
+;    xor ecx, ecx            ; счетчик фигур
+;    movsd xmm1, [.sum]      ; перенос накопителя суммы в регистр 1
+;.loop:
+;    cmp ecx, ebx            ; проверка на окончание цикла
+;    jge .return             ; Перебрали все фигуры
+;
+;    mov r10, rdi            ; сохранение начала фигуры
+;    call IdealTimeTransport     ; Получение периметра первой фигуры
+;    addsd xmm1, xmm0        ; накопление суммы
+;    inc rcx                 ; индекс следующей фигуры
+;    add r10, 24             ; адрес следующей фигуры
+;    mov rdi, r10            ; восстановление для передачи параметра
+;    jmp .loop
+.return:
+    movsd xmm0, xmm1
+leave
+ret
---
+++ 11-asm/output.asm
@@ -0,0 +1,290 @@
+; file.asm - использование файлов в NASM
+extern printf
+extern fprintf
+
+extern IdealTimeTrain
+extern IdealTimeShip
+extern IdealTimePlane
+
+extern TRAIN
+extern SHIP
+extern PLANE
+
+;----------------------------------------------
+;void Out(train &t, ofstream &ofst) {
+;    ofst << "It is a train: number of railway carriage = "
+;         << t.numberOfRailwayCarriage << ", speed = " << t.speed
+;         << ", distance = " << t.distance
+;         << ". Ideal time = " << IdealTime(t) << "\n";
+;}
+global OutTrain
+OutTrain:
+section .data
+    .outfmt db "It is a train: number of railway carriage = %d, speed = %d, distance = %lg. Ideal time = %lg",10,0
+section .bss
+    .prect  resq  1
+    .FILE   resq  1       ; временное хранение указателя на файл
+    .p      resq  1       ; вычисленный периметр прямоугольника
+section .text
+push rbp
+mov rbp, rsp
+
+    ; Сохранени принятых аргументов
+    mov     [.prect], rdi          ; сохраняется адрес поезда
+    mov     [.FILE], rsi          ; сохраняется указатель на файл
+
+    ; Вычисление идеального времени поезда (адрес уже в rdi)
+    mov     rdi, [.prect]
+    call    IdealTimeTrain
+    movsd   [.p], xmm0          ; сохранение (может лишнее) идельного времени
+
+    ; Вывод информации о поезде в консоль
+;     mov     rdi, .outfmt        ; Формат - 1-й аргумент
+;     mov     rax, [.prect]       ; адрес прямоугольника
+;     mov     esi, [rax]          ; x
+;     mov     edx, [rax+4]        ; y
+;     movsd   xmm0, [.p]
+;     mov     rax, 1              ; есть числа с плавающей точкой
+;     call    printf
+
+    ; Вывод информации о поезде в файл
+    mov     rdi, [.FILE]
+    mov     rsi, .outfmt        ; Формат - 2-й аргумент
+    mov     rax, [.prect]        ; адрес поезда
+    mov     edx, [rax]          ; x
+    mov     ecx, [rax+4]        ; y
+    movsd   xmm0, [rax+8]
+    movsd   xmm1, [.p]
+    mov     rax, 1              ; есть числа с плавающей точкой
+    call    fprintf
+
+leave
+ret
+
+;----------------------------------------------
+; // Вывод параметров корабля в файл
+;void Out(ship &s, ofstream &ofst) {
+;    ofst << "It is a ship: displacement = "
+;         << s.displacement << ", type = " << s.type << ", speed = " << s.speed
+;         << ", distance = " << s.distance
+;         << ". Ideal time = " << IdealTime(s) << "\n";
+;}
+global OutShip
+OutShip:
+section .data
+    .outfmt db "It is a ship: type = %d, displacement =  %d, speed = %d, distance =  %lg. Ideal time = %lg",10,0
+section .bss
+    .ptrian  resq  1
+    .FILE   resq  1       ; временное хранение указателя на файл
+    .p      resq  1       ; вычисленный периметр треугольника
+section .text
+push rbp
+mov rbp, rsp
+
+    ; Сохранени принятых аргументов
+    mov     [.ptrian], rdi        ; сохраняется адрес ship
+    mov     [.FILE], rsi          ; сохраняется указатель на файл
+
+    ; Вычисление периметра треугольника (адрес уже в rdi)
+    call    IdealTimeShip
+    movsd   [.p], xmm0          ; сохранение (может лишнее) периметра
+
+    ; Вывод информации о треугольнике в консоль
+;     mov     rdi, .outfmt        ; Формат - 1-й аргумент
+;     mov     rax, [.ptrian]       ; адрес треугольника
+;     mov     esi, [rax]          ; a
+;     mov     edx, [rax+4]        ; b
+;     mov     ecx, [rax+8]        ; c
+;     movsd   xmm0, [.p]
+;     mov     rax, 1              ; есть числа с плавающей точкой
+;     call    printf
+
+    ; Вывод информации о ship в файл
+    mov     rdi, [.FILE]
+    mov     rsi, .outfmt        ; Формат - 2-й аргумент
+    mov     rax, [.ptrian]      ; адрес треугольника
+    mov     rcx,[rax]          ; 0 int displacement
+    mov     rdx, [rax+4]        ; +4 int type
+    mov     r8, [rax+8]       ; +4+4 int speed
+    movsd   xmm0, [rax+12]    ; +4+4+4 double distance
+    movsd   xmm1, [.p]
+    mov     rax, 1              ; есть числа с плавающей точкой
+    call    fprintf
+
+leave
+ret
+
+;// Вывод параметров самолета в поток
+;void Out(plane &p, ofstream &ofst) {
+;    ofst << "It is a plane: flight range = "
+;         << p.flightRange << ", lifting capacity = " << p.liftingCapacity << ", speed = " << p.speed
+;         << ", distance = " << p.distance
+;         << ". Ideal time = " << IdealTime(p) << "\n";
+;}
+global OutPlane
+OutPlane:
+section .data
+    .outfmt db "It is a plane: flight range =  %d, lifting capacity = %d, speed = %d, distance = %lg. Ideal time = %lg",10,0
+section .bss
+    .prect  resq  1
+    .FILE   resq  1       ; временное хранение указателя на файл
+    .p      resq  1       ; вычисленный периметр прямоугольника
+section .text
+push rbp
+mov rbp, rsp
+
+    ; Сохранени принятых аргументов
+    mov     [.prect], rdi          ; сохраняется адрес поезда
+    mov     [.FILE], rsi          ; сохраняется указатель на файл
+
+    ; Вычисление идеального времени поезда (адрес уже в rdi)
+    mov     rdi, [.prect]
+    call    IdealTimePlane
+    movsd   [.p], xmm0          ; сохранение (может лишнее) идельного времени
+
+    ; Вывод информации о поезде в консоль
+;     mov     rdi, .outfmt        ; Формат - 1-й аргумент
+;     mov     rax, [.prect]       ; адрес прямоугольника
+;     mov     esi, [rax]          ; x
+;     mov     edx, [rax+4]        ; y
+;     movsd   xmm0, [.p]
+;     mov     rax, 1              ; есть числа с плавающей точкой
+;     call    printf
+
+    ; Вывод информации о поезде в файл
+    mov     rdi, [.FILE]
+    mov     rsi, .outfmt        ; Формат - 2-й аргумент
+    mov     rax, [.prect]        ; адрес поезда
+    mov     rdx, [rax]          ; x
+    mov     rcx, [rax+4]        ; y
+    mov     r8, [rax+8]
+    movsd   xmm0, [rax+12]
+    movsd   xmm1, [.p]
+    mov     rax, 1              ; есть числа с плавающей точкой
+    call    fprintf
+
+leave
+ret
+;----------------------------------------------
+; // Вывод параметров текущей фигуры в файл
+; void OutTransport(void *s, FILE *ofst) {
+;     int k = *((int*)s);
+;     if(k == RECTANGLE) {
+;         OutRectangle(s+intSize, ofst);
+;     }
+;     else if(k == TRIANGLE) {
+;         OutShip(s+intSize, ofst);
+;     }
+;     else {
+;         fprintf(ofst, "Incorrect figure!\n");
+;     }
+; }
+global OutTransport
+OutTransport:
+section .data
+    .erShape db "Incorrect figure!",10,0
+    .outf db "type transoirt  = %d",10,0
+section .text
+push rbp
+mov rbp, rsp
+
+    ; В rdi адрес фигуры
+    mov eax, [rdi]
+    
+;    mov rsi, [rdi]
+;    mov rdi, .outf
+;    mov rax , 0
+;    call printf
+;    mov [rdi], rsi
+;    
+    
+    cmp eax, [TRAIN]
+    je trainOut
+    cmp eax, [SHIP]
+    je shipOut
+    cmp eax, [PLANE]
+    je planeOut
+    mov rdi, .erShape
+    mov rax, 0
+    call fprintf
+    jmp     return
+trainOut:
+    ; Вывод train
+    add     rdi, 4
+    call    OutTrain
+    jmp     return
+shipOut:
+    ; Вывод ship
+    add     rdi, 4
+    call    OutShip
+    jmp     return
+planeOut:
+    add     rdi, 4
+    call    OutPlane
+    jmp     return
+return:
+leave
+ret
+
+;----------------------------------------------
+; // Вывод содержимого контейнера в файл
+; void OutContainer(void *c, int len, FILE *ofst) {
+;     void *tmp = c;
+;     fprintf(ofst, "Container contains %d elements.\n", len);
+;     for(int i = 0; i < len; i++) {
+;         fprintf(ofst, "%d: ", i);
+;         OutTransport(tmp, ofst);
+;         tmp = tmp + shapeSize;
+;     }
+; }
+global OutContainer
+OutContainer:
+section .data
+    numFmt  db  "%d: ",0
+section .bss
+    .pcont  resq    1   ; адрес контейнера
+    .len    resd    1   ; адрес для сохранения числа введенных элементов
+    .FILE   resq    1   ; указатель на файл
+section .text
+push rbp
+mov rbp, rsp
+
+    mov [.pcont], rdi   ; сохраняется указатель на контейнер
+    mov [.len],   esi     ; сохраняется число элементов
+    mov [.FILE],  rdx    ; сохраняется указатель на файл
+
+    ; В rdi адрес начала контейнера
+    mov rbx, rsi            ; число фигур
+    xor ecx, ecx            ; счетчик фигур = 0
+    mov rsi, rdx            ; перенос указателя на файл
+.loop:
+    cmp ecx, ebx            ; проверка на окончание цикла
+    jge .return             ; Перебрали все фигуры
+
+    push rbx
+    push rcx
+
+    ; Вывод номера фигуры
+    mov     rdi, [.FILE]    ; текущий указатель на файл
+    mov     rsi, numFmt     ; формат для вывода фигуры
+    mov     edx, ecx        ; индекс текущей фигуры
+    xor     rax, rax,       ; только целочисленные регистры
+    call fprintf
+
+    ; Вывод текущей фигуры
+    mov     rdi, [.pcont]
+    mov     rsi, [.FILE]
+    call OutTransport     ; Получение периметра первой фигуры
+
+    pop rcx
+    pop rbx
+    inc ecx                 ; индекс следующей фигуры
+
+    mov     rax, [.pcont]
+    add     rax, 24         ; адрес следующей фигуры
+    mov     [.pcont], rax
+    jmp .loop
+.return:
+leave
+ret
+
